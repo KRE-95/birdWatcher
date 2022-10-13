15217d001a6405f82a9ed1e5e758babc50e47593
Marianne GO <mariagol@hiof.no>
2022-10-13 21:51:04 +0200

    Started adding camera

--- app/src/main/java/com/gruppe16/birdwatcher/HomeFragment.kt
+++ app/src/main/java/com/gruppe16/birdwatcher/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,6 +21,8 @@ class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    private fun startCamera() {}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/gruppe16/birdwatcher/MainActivity.kt
+++ app/src/main/java/com/gruppe16/birdwatcher/MainActivity.kt
@@ -1,29 +1,44 @@
 package com.gruppe16.birdwatcher
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.navigation.NavigationView
+import com.gruppe16.birdwatcher.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     // global (not null) this for navigation drawer - connect three navigation coponents.
     lateinit var drawerOpen: ImageView
     lateinit var navigationDrawer: NavigationView
     lateinit var drawerLayout: DrawerLayout
-
+    private lateinit var viewBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
         // from activity_main xml - for navigation drawer
         drawerOpen=findViewById(R.id.drawer_open)
         navigationDrawer=findViewById(R.id.nav_drawer)
         drawerLayout=findViewById(R.id.drawer_layout)
+
+        //Permission
+        if (allPermissionsGranted()) {
+            HomeFragment.startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
 
         // when click
         drawerOpen.setOnClickListener {
@@ -32,7 +47,28 @@ class MainActivity : AppCompatActivity() {
         // to connect
         val navController:NavController=Navigation.findNavController(this,R.id.fragment)
         NavigationUI.setupWithNavController(navigationDrawer,navController)
-
-
     }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    companion object {
+        private const val TAG = "Bird Watcher"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf (
+                Manifest.permission.CAMERA
+            ).apply {
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                }
+            }.toTypedArray()
+    }
+}
+
+private fun HomeFragment.Companion.startCamera() {
+    TODO("Not yet implemented")
 }
