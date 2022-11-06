3a4c7b3878d42eb1bcd682561f72f2646ca733f1
erikvs <89622099+erikvs@users.noreply.github.com>
2022-11-06 19:29:01 +0100

    Bound SelectedItemFragment
    - missing logic for edit activity

--- app/src/main/java/com/gruppe16/birdwatcher/SelectedItemFragment.kt
+++ app/src/main/java/com/gruppe16/birdwatcher/SelectedItemFragment.kt
@@ -5,6 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.gruppe16.birdwatcher.databinding.FragmentSelecteditemBinding
+
+// View binding fragments
+private var _binding: FragmentSelecteditemBinding? = null
+private val binding get() = _binding!!
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,7 +22,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [SelectedItemFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SelectedItemFragment : Fragment() {
+class SelectedItemFragment : Fragment(R.layout.fragment_selecteditem) {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -30,30 +36,15 @@ class SelectedItemFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_selecteditem, container, false)
+        _binding = FragmentSelecteditemBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SelectedItemFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            GlobalFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+    //TODO: add logic for edit activity and link to button
+    private fun editSelectedItem(){}
 }
