7db68a6cc844ab8fb1625fa0466dc3242a31d07d
erikvs <89622099+erikvs@users.noreply.github.com>
2022-11-09 22:47:39 +0100

    Attempt at DB -> RecyclerView
    bekreftet at data lastes inn fra databasen og ligger lokalt i ArrayList med objekter av typen Listing. Får ingen visning.

--- app/src/main/java/com/gruppe16/birdwatcher/GalleryFragment.kt
+++ app/src/main/java/com/gruppe16/birdwatcher/GalleryFragment.kt
@@ -1,147 +1,74 @@
 package com.gruppe16.birdwatcher
-
 import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.firestore.FirebaseFirestore
 import com.gruppe16.birdwatcher.adapter.GalleryRecyclerAdapter
 import com.gruppe16.birdwatcher.data.Listing
-import com.gruppe16.birdwatcher.databinding.FragmentGalleryBinding
-
 
 class GalleryFragment : Fragment(R.layout.fragment_gallery) {
-
- // disse binder fragment til .kt
-private var _binding : FragmentGalleryBinding? = null
-private val bindingGalleryFragment get() = _binding!!
-
 
 private lateinit var ourAdapter: GalleryRecyclerAdapter
 private lateinit var recyclerView: RecyclerView
 private lateinit var galleryArrayList : ArrayList<Listing>
-var db = FirebaseFirestore.getInstance()
+private lateinit var db : FirebaseFirestore
 
-
- //TODO: Disse to variablene er allerede i en listing.
- lateinit var picture : Array<String>
- lateinit var birdName: Array<String>
- //lateinit var description: Array<String>
-
- override fun onCreate(savedInstanceState: Bundle?) {
-  super.onCreate(savedInstanceState)
-
-
-// recyclerView = requireView().findViewById(R.id.RecyclerView)
-  //recyclerView.layoutManager = LinearLayoutManager(this)
- // recyclerView.hasFixedSize()
-  //recyclerView.layoutManager = LinearLayoutManager(this) //TODO: define context?
-
-  //galleryArrayList = arrayListOf()
-
- // adapter = GalleryRecyclerAdapter(galleryArrayList)
-
- // recyclerView.adapter = adapter
- }
-
-
- //denne fungerer, men ingen visning enda //TODO: Denne het onCreateView før
+ //denne fungerer, men ingen visning enda
  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 super.onViewCreated(view, savedInstanceState)
-  dataInitialize()
-/*  inflater: LayoutInflater,
-  container: ViewGroup?,
-  savedInstanceState: Bundle?
- ): View? {
-  _binding = FragmentGalleryBinding.inflate(inflater, container, false)*/
 
-  //galleryArrayList = arrayListOf()
-//TODO: Henter data og skriver ut navn på fugler i galleryArrayList - bør kanskje flyttes?
- // getDataFromFirestore()
+galleryArrayList = arrayListOf()
 
 val layoutManager = LinearLayoutManager(context)
-  recyclerView = view.findViewById(R.id.RecyclerView)
-  recyclerView.layoutManager = layoutManager
- recyclerView.hasFixedSize()
-  ourAdapter = GalleryRecyclerAdapter(galleryArrayList)
- recyclerView.adapter = ourAdapter
+recyclerView = view.findViewById(R.id.RecyclerView)
+recyclerView.layoutManager = layoutManager
+//recyclerView.hasFixedSize()
+ourAdapter = GalleryRecyclerAdapter(galleryArrayList)
+recyclerView.adapter = ourAdapter
 
-  //return bindingGalleryFragment.root
+getDataFromFirestore()
  }
-
-
 
  override fun onDestroyView() {
   super.onDestroyView()
   //TODO: Benyttes kun for test, sjekker hvor mange fugler som ligger i galleriet når det ødelegges
   println(galleryArrayList.size)
-  _binding = null
  }
 
+private fun getDataFromFirestore(){
 
- private fun dataInitialize(){
+ db = FirebaseFirestore.getInstance()
+ db.collection("listings")//TODO Add query to sort by descending?
+  .get()
+  .addOnCompleteListener { task ->
+   if (task.isSuccessful) {
+    for (document in task.result) {
+     Log.d(TAG, document.id + " => " + document.data)
 
-  galleryArrayList = arrayListOf<Listing>()
-
-  picture = arrayOf(
-   R.drawable.fossekallmain
-    .toString())
-
-  birdName = arrayOf(
-   getString(R.string.birdName_1)
-  )
-
-
-
-  for (i in picture.indices){
-
-   val listing = Listing(birdName[i]) //picture[i]
-   galleryArrayList.add(listing)
-
-  }
-
- }
-
-
-/* fun getDataFromFirestore(){
-
-  db.collection("listings")//TODO Add query to sort by descending?
-   .get()
-   .addOnCompleteListener { task ->
-    if (task.isSuccessful) {
-     for (document in task.result) {
-      Log.d(TAG, document.id + " => " + document.data)
-
-      //For hvert leste element legges elementet til i lokal lagring
-      val newObject = document.toObject(Listing::class.java)
+     //For hvert leste objekt legges det til i lokal lagring
+     val newObject = document.toObject(Listing::class.java)
      galleryArrayList.add(newObject)
-     }
-   //  ourAdapter.notifyDataSetChanged()
-    } else {
-     Log.w(TAG, "Error getting documents.", task.exception)
+
+     //TODO: FOR TESTING
+     println("******************************************************")
+     println("${newObject.birdName}, ${newObject.description}")
+     println("******************************************************")
     }
-
-    }
-
-
- }*/
+   } else {
+    Log.w(TAG, "Error getting documents.", task.exception)
+   }
+  }
+}
+}
 
 //TODO: JUST NOTES
-    // lage front-end på gallery - done
-
-    // Koble recyleview med items fra layout done
-
-    // lage mappe kalt adapter - done
-
-    // i adapter koble alt info av list item osv
- // sett opp RecycleView_galley
-
-    // hente bilde/text fra data Listing og koble til Firebase?
+// Få tak på data som ligger lokalt etter databasekall og føre det til recyclerView
 
 
-}
+
+
+
--- app/src/main/java/com/gruppe16/birdwatcher/adapter/GalleryRecyclerAdapter.kt
+++ app/src/main/java/com/gruppe16/birdwatcher/adapter/GalleryRecyclerAdapter.kt
@@ -3,26 +3,22 @@ package com.gruppe16.birdwatcher.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.imageview.ShapeableImageView
 import com.gruppe16.birdwatcher.R
 import com.gruppe16.birdwatcher.data.Listing
-
 
 class GalleryRecyclerAdapter (private val galleryListing : ArrayList<Listing> ): RecyclerView.Adapter<GalleryRecyclerAdapter.ViewHolder>(){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val  itemView = LayoutInflater.from(parent.context).inflate(R.layout.gallery_list_item, parent, false)
         return ViewHolder(itemView)
-
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val currentItem = galleryListing[position]
 
-        //holder.picture.text = currentItem.picture
+      //  holder.picture.text = currentItem.picture
         holder.birdName.text = currentItem.birdName
         holder.description.text = currentItem.description
 
@@ -37,7 +33,7 @@ class GalleryRecyclerAdapter (private val galleryListing : ArrayList<Listing> ):
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
      // gallery_list + listing
 
-       // val picture: TextView = itemView.findViewById(R.id.ImageV_Display_Gallery)
+        //val picture: ShapeableImageView = itemView.findViewById(R.id.ImageV_Display_Gallery) //TODO: see if you can rework the view to have this as a textview and just use string?
         val birdName: TextView = itemView.findViewById(R.id.tV_BirdName)
         val description: TextView = itemView.findViewById(R.id.tV_Description_Gallery)
 
