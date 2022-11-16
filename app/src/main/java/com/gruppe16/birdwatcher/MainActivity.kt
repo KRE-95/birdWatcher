430e734fe81ba75d569131dc1ce3ce5e00fdf8df
erikvs <89622099+erikvs@users.noreply.github.com>
2022-11-16 18:05:38 +0100

    location data in HomeFragment
    longitude and latitude present in backend, need to convert this to string to store in data class

--- app/src/main/java/com/gruppe16/birdwatcher/MainActivity.kt
+++ app/src/main/java/com/gruppe16/birdwatcher/MainActivity.kt
@@ -1,21 +1,18 @@
 package com.gruppe16.birdwatcher
 
-import android.Manifest
 import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
 import android.widget.ImageView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.material.navigation.NavigationView
-import com.gruppe16.birdwatcher.data.User
 import com.gruppe16.birdwatcher.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -24,6 +21,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var navigationDrawer: NavigationView
     lateinit var drawerLayout: DrawerLayout
     private lateinit var viewBinding: ActivityMainBinding
+    //private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,7 +39,30 @@ class MainActivity : AppCompatActivity() {
         // to connect
         val navController:NavController=Navigation.findNavController(this,R.id.fragment)
         NavigationUI.setupWithNavController(navigationDrawer,navController)
+
+        // LocationProvider
+       // fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+       // getLocation()
     }
+
+    /*private fun getLocation() {
+        val task = fusedLocationProviderClient.lastLocation
+
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED && ActivityCompat
+                .checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        ){
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 101)
+            return
+        }
+        task.addOnSuccessListener {
+            if(it != null){
+                println("*******************TESTING*******************")
+                println("${it.latitude}, ${it.longitude}")
+                println("*******************TESTING*******************")
+            }
+        }
+    }*/
 
 }
 
--- app/src/main/java/com/gruppe16/birdwatcher/fragments/HomeFragment.kt
+++ app/src/main/java/com/gruppe16/birdwatcher/fragments/HomeFragment.kt
@@ -1,7 +1,9 @@
 package com.gruppe16.birdwatcher.fragments
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
@@ -9,10 +11,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.constraintlayout.motion.widget.Debug.getLocation
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.gruppe16.birdwatcher.R
 import com.gruppe16.birdwatcher.components.CameraX
 import com.gruppe16.birdwatcher.databinding.FragmentHomeBinding
@@ -27,6 +32,8 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     private val binding get() = _binding!!
     private lateinit var cameraExecutor: ExecutorService
     private lateinit var camera: CameraX
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,6 +49,11 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         if (allPermissionsGranted()) {
             camera = CameraX(this, binding)
             camera.startCamera()
+            // LocationProvider
+            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(activity as Context)
+            //getLocation()
+
+
         } else {
             ActivityCompat.requestPermissions(
                 activity as Activity, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
@@ -52,6 +64,8 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
             camera.takePhoto(this, binding)
             viewModel.setPictureUri(camera.pictureUri)
             viewModel.setDate(camera.date)
+            getLocation()
+           //TODO: use longitude and latitude here to define the place and send that info to createdItem
         }
         cameraExecutor = Executors.newSingleThreadExecutor()
     }
@@ -65,13 +79,16 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         private const val REQUEST_CODE_PERMISSIONS = 10
         private val REQUIRED_PERMISSIONS =
             mutableListOf (
-                Manifest.permission.CAMERA
+                //TODO: COULD ALL CHECKS BE DONE HERE instead of an own check for location in getLocation ?
+                Manifest.permission.CAMERA //, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION
             ).apply {
                 if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
                     add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 }
             }.toTypedArray()
     }
+
+
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(
@@ -84,12 +101,34 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         if (requestCode == REQUEST_CODE_PERMISSIONS) {
             if (allPermissionsGranted()) {
                 camera.startCamera()
+
             } else {
                 Toast.makeText(
                     context,
                     "Permissions not granted by the user.",
                     Toast.LENGTH_SHORT
                 ).show()
+            }
+        }
+    }
+
+
+   private fun getLocation() {
+        val task = fusedLocationProviderClient.lastLocation
+
+        if(ActivityCompat.checkSelfPermission(activity as Context, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED && ActivityCompat
+                .checkSelfPermission(activity as Context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        ){
+            ActivityCompat.requestPermissions(context as Activity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 101)
+            return
+        }
+        task.addOnSuccessListener {
+            if(it != null){
+                //TODO: REMOVE TEST PRINTING
+                println("*******************TESTING*******************")
+                println("${it.latitude}, ${it.longitude}")
+                println("*******************TESTING*******************")
             }
         }
     }
