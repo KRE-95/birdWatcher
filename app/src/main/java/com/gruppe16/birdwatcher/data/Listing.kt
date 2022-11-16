e7106346738ce9806a636be1c31b69010ce27d06
Marianne GO <mariagol@hiof.no>
2022-11-16 12:02:01 +0100

    Deleted id from listing, its never used

--- app/src/main/java/com/gruppe16/birdwatcher/data/Listing.kt
+++ app/src/main/java/com/gruppe16/birdwatcher/data/Listing.kt
@@ -1,11 +1,8 @@
 package com.gruppe16.birdwatcher.data
 
 data class Listing (
-    // rekkefølge må være lik firebase
-
     var birdName: String = "",
     var description: String = "",
-    //var listingId: Int = 0,
     var picture: String = "",
     //var place: String = "",
     var date: String = "",
