c99672ee275dbadbd446101e0867a5c7bf8001c1
Marianne GO <mariagol@hiof.no>
2022-11-19 18:58:50 +0100

    Some formatting

--- app/src/main/java/com/gruppe16/birdwatcher/fragments/GalleryFragment.kt
+++ app/src/main/java/com/gruppe16/birdwatcher/fragments/GalleryFragment.kt
@@ -20,83 +20,79 @@ import com.gruppe16.birdwatcher.viewmodels.SharedViewModel
 
 
 class GalleryFragment : Fragment(), GalleryRecyclerAdapter.OnListingClickedListener {
- private val viewModel: SharedViewModel by activityViewModels()
- private var _binding : FragmentGalleryBinding? = null
- private val binding get() = _binding!!
- private lateinit var ourAdapter: GalleryRecyclerAdapter
- private lateinit var recyclerView: RecyclerView
- private lateinit var galleryArrayList : ArrayList<Listing>
- private lateinit var idList: ArrayList<String>
- private lateinit var db : FirebaseFirestore
 
- override fun onCreateView(
-  inflater: LayoutInflater, container: ViewGroup?,
-  savedInstanceState: Bundle?
- ): View? {
-  _binding = FragmentGalleryBinding.inflate(inflater, container, false)
-  return binding.root
- }
+    private val viewModel: SharedViewModel by activityViewModels()
+    private var _binding : FragmentGalleryBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var ourAdapter: GalleryRecyclerAdapter
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var galleryArrayList : ArrayList<Listing>
+    private lateinit var idList: ArrayList<String>
+    private lateinit var db : FirebaseFirestore
 
- //denne fungerer, men ingen visning enda
- override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-  super.onViewCreated(view, savedInstanceState)
-  galleryArrayList = arrayListOf()
-  idList = arrayListOf()
-  getDataFromFirestore()
-
-  val layoutManager = LinearLayoutManager(context)
-  recyclerView = view.findViewById(R.id.RecyclerView)
-  recyclerView.layoutManager = layoutManager
-  recyclerView.hasFixedSize()
-  ourAdapter = GalleryRecyclerAdapter(galleryArrayList, this)
-  recyclerView.adapter = ourAdapter
- }
-
- override fun onDestroyView() {
-  super.onDestroyView()
-  //TODO: Benyttes kun for test, sjekker hvor mange fugler som ligger i galleriet når det ødelegges
-  println(galleryArrayList.size)
- }
-
-private fun getDataFromFirestore() {
-
- db = FirebaseFirestore.getInstance()
- db.collection("listings")//TODO Add query to sort by descending?
-  .get()
-  .addOnCompleteListener { task ->
-   if (task.isSuccessful) {
-    for (document in task.result) {
-     Log.d(TAG, document.id + " => " + document.data)
-
-     //For hvert leste objekt legges det til i lokal lagring
-     val newObject = document.toObject(Listing::class.java)
-     galleryArrayList.add(newObject)
-     idList.add(document.id)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        return binding.root
     }
-    ourAdapter.notifyDataSetChanged()
-   } else {
-    Log.w(TAG, "Error getting documents.", task.exception)
-   }
-  }
-}
 
- override fun onListingClicked(position: Int) {
-  val clicked = galleryArrayList[position]
-  Log.d("CLICKED ID", "HERE ${idList[position]}")
-  viewModel.setBirdName(clicked.birdName)
-  viewModel.setDescription(clicked.description)
-  viewModel.setPicture(clicked.picture)
-  viewModel.setUser(clicked.user)
-  viewModel.setDate(clicked.date)
-  viewModel.setListId(idList[position])
-  findNavController().navigate(R.id.action_galleryFragment_to_selectedItemFragment)
- }
+    //denne fungerer, men ingen visning enda
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        galleryArrayList = arrayListOf()
+        idList = arrayListOf()
+        getDataFromFirestore()
+
+        val layoutManager = LinearLayoutManager(context)
+        recyclerView = view.findViewById(R.id.RecyclerView)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.hasFixedSize()
+        ourAdapter = GalleryRecyclerAdapter(galleryArrayList, this)
+        recyclerView.adapter = ourAdapter
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        //TODO: Benyttes kun for test, sjekker hvor mange fugler som ligger i galleriet når det ødelegges
+        println(galleryArrayList.size)
+    }
+
+    private fun getDataFromFirestore() {
+        db = FirebaseFirestore.getInstance()
+        db.collection("listings")//TODO Add query to sort by descending?
+            .get()
+            .addOnCompleteListener { task ->
+             if (task.isSuccessful) {
+                 for (document in task.result) {
+                    Log.d(TAG, document.id + " => " + document.data)
+
+                    //For hvert leste objekt legges det til i lokal lagring
+                    val newObject = document.toObject(Listing::class.java)
+                    galleryArrayList.add(newObject)
+                    idList.add(document.id)
+                 }
+                 ourAdapter.notifyDataSetChanged()
+             } else {
+                 Log.w(TAG, "Error getting documents.", task.exception)
+             }
+        }
+    }
+
+    override fun onListingClicked(position: Int) {
+        val clicked = galleryArrayList[position]
+        Log.d("CLICKED ID", "HERE ${idList[position]}")
+        viewModel.setBirdName(clicked.birdName)
+        viewModel.setDescription(clicked.description)
+        viewModel.setPicture(clicked.picture)
+        viewModel.setUser(clicked.user)
+        viewModel.setDate(clicked.date)
+        viewModel.setListId(idList[position])
+        findNavController().navigate(R.id.action_galleryFragment_to_selectedItemFragment)
+    }
 }
 
 //TODO: JUST NOTES
 // Få tak på data som ligger lokalt etter databasekall og føre det til recyclerView
-
-
-
-
 
--- app/src/main/java/com/gruppe16/birdwatcher/fragments/SelectedItemFragment.kt
+++ app/src/main/java/com/gruppe16/birdwatcher/fragments/SelectedItemFragment.kt
@@ -118,3 +118,4 @@ class SelectedItemFragment : Fragment() {
         }
     }
 }
+
